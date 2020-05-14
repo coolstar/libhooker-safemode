0df4c2f72f2f35cdec38698a288f2c9ae2e6ba3a
CoolStar <coolstarorganization@gmail.com>
2020-05-14 04:01:02 -0700

    Add Button Highlight

--- SafeMode/RespringButton.m
+++ SafeMode/RespringButton.m
@@ -18,6 +18,14 @@
     return self;
 }
 
+- (void)setHighlighted:(BOOL)highlighted {
+    if (highlighted){
+        [self setBackgroundColor:[UIColor colorNamed:@"ButtonHighlightedColor"]];
+    } else {
+        [self setBackgroundColor:[UIColor colorNamed:@"ButtonColor"]];
+    }
+}
+
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
